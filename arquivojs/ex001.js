865696b008bcda3fa9926dfa2da82ba627200af2
anaback <anabackmsb@gmail.com>
2023-10-29 14:44:51 -0300

    importados arquivos CSS e JS

---
+++ arquivojs/ex001.js
@@ -0,0 +1,4 @@
+        window.alert('Minha primeira menssagem!') 
+        window.confirm('Está gostando de Js?') // janela com botão ok e cancelar
+        window.prompt('Qual é o seu nome?') //vai perguntar o nome
+   
