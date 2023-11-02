f2530a965e5ce9aec305016c894cdff52412e554
anaback <anabackmsb@gmail.com>
2023-11-02 15:54:37 -0300

    criando repetições

---
+++ repeticoes/ambiente.js
@@ -0,0 +1,23 @@
+var contador = 1
+
+// do = faça
+// while = enquanto
+
+do {
+    console.log(`repetir ${contador}`)
+    contador++
+} while (contador <= 6)
+
+// var contador = 1 
+// while (contador <= 6) {
+//     console.log(`repetir ${contador}`)
+//     contador++
+// }
+
+/*
+console.log('repetir.')
+console.log('repetir.')
+console.log('repetir.')
+console.log('repetir.')
+console.log('repetir.')
+console.log('repetir.') */
---
+++ repeticoes01/ambiente.js
@@ -0,0 +1,12 @@
+
+// var contador = 1
+// while (contador <=10) {
+//     console.log(`repetir ${contador}.`)
+//     contador++
+// }
+
+//-------agora usando o for----------------------------------------
+
+for(var contador = 1; contador <= 10; contador++ ) {
+    console.log(`repetir ${contador}.`)
+}
